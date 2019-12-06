c7f8474ba4f140361b438261f4d66bedb788f5c5
A-Charvin <anandcharvin@yahoo.com>
2019-12-06 16:51:47 +0530

    Changed to english

--- wakfu-gear/src/language/lang.js
+++ wakfu-gear/src/language/lang.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import { ptBR } from './ptBR'
-
+import { enUS } from './enUS'
 Vue.use(VueI18n)
 
 export const i18n = new VueI18n({
-  locale: 'pt-BR',
-  messages: { 'pt-BR': ptBR }
+  locale: 'en-US',
+  messages: { 'en-US': enUS }
 })
