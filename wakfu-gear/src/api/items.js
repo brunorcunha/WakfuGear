0aa472761507ce39ee9aed2c9d7a3fad67a3d7ce
Bruno Cunha <brunorcunha@hotmail.com>
2020-04-26 21:47:46 -0300

    Troca da url do netlify

--- wakfu-gear/src/api/items.js
+++ wakfu-gear/src/api/items.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-const endpoint = 'https://wakfu-api.netlify.com/.netlify/functions'
+const endpoint = 'https://wakfu-api.netlify.app/.netlify/functions'
 
 export default {
   getItems: async () => (await Vue.http.get(`${endpoint}/items`)).body,
--- wakfu-gear/src/api/linker.js
+++ wakfu-gear/src/api/linker.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-const endpoint = 'https://wakfu-api.netlify.com/.netlify/functions'
+const endpoint = 'https://wakfu-api.netlify.app/.netlify/functions'
 
 export default {
   get: async (resource, params) => (await Vue.http.post(`${endpoint}/linker`, { resource, params })).body
