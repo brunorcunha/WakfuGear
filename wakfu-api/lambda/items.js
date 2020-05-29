3968d5375247171e4f6228180e745e19f8aecf61
Bruno Cunha <brunorcunha@hotmail.com>
2020-05-29 09:56:39 -0300

    Merging effects with a negative ID

--- wakfu-api/lambda/items.js
+++ wakfu-api/lambda/items.js
@@ -60,29 +60,74 @@ const verificarGFX = item => {
   return `${iid}` === `${type}${id}`
 }
 
+const formatarEquipEffects = equipEffects => {
+  const IIDtoID = [
+    { iid: 21, id: 20 },
+    { iid: 32, id: 31 },
+    { iid: 56, id: 31 },
+    { iid: 57, id: 41 },
+    { iid: 1063, id: 71 },
+    { iid: 90, id: 80 },
+    { iid: 100, id: 80 },
+    { iid: 93, id: 82 },
+    { iid: 94, id: 83 },
+    { iid: 92, id: 84 },
+    { iid: 130, id: 120 },
+    { iid: 132, id: 122 },
+    { iid: 1056, id: 149 },
+    { iid: 168, id: 150 },
+    { iid: 161, id: 160 },
+    { iid: 167, id: 166 },
+    { iid: 172, id: 171 },
+    { iid: 174, id: 173 },
+    { iid: 176, id: 175 },
+    { iid: 181, id: 180 },
+    { iid: 192, id: 191 },
+    { iid: 194, id: 191 },
+    { iid: 876, id: 875 },
+    { iid: 1060, id: 1053 },
+    { iid: 1061, id: 1055 }
+  ]
+  const ArrayIID = IIDtoID.map(e => e.iid)
+  const tID = fx.effect.definition.actionId
+  let params = fx.effect.definition.params
+  let id = tID
+
+  if (ArrayIID.includes(tID)) {
+    id = IIDtoID.find(e => e.iid === tID).id
+    params[0] *= -1
+  }
+
+  return equipEffects.map(fx => ({ id, params }))
+}
+
 const formatarItem = item => {
-  let iid = [ item.definition.item.graphicParameters.gfxId ]
-  let gfx = item.definition.item.graphicParameters.femaleGfxId
-  if (iid[0] !== gfx) iid.push(gfx)
+  const id = item.definition.item.id
+  const title = item.title
+  const set = item.definition.item.baseParameters.itemSetId
+  const gfx = item.definition.item.graphicParameters.femaleGfxId
+  const type = item.definition.item.baseParameters.itemTypeId
+  const rarity = item.definition.item.baseParameters.rarity
+  const minShard = item.definition.item.baseParameters.minimumShardSlotNumber
+  const maxShard = item.definition.item.baseParameters.maximumShardSlotNumber
+  const shard = [minShard, maxShard]
   let lvl = item.definition.item.level
-  let type = item.definition.item.baseParameters.itemTypeId
+  let iid = [ item.definition.item.graphicParameters.gfxId ]
+  if (iid[0] !== gfx) iid.push(gfx)
   if ([582, 420].includes(type)) lvl = 50
+
+  const equipEffects = formatarEquipEffects(item.definition.equipEffects)
+
   return {
-    id: item.definition.item.id,
+    id,
     iid,
-    title: item.title,
+    title,
     lvl,
     type,
-    set: item.definition.item.baseParameters.itemSetId,
-    rarity: item.definition.item.baseParameters.rarity,
-    shard: [
-      item.definition.item.baseParameters.minimumShardSlotNumber,
-      item.definition.item.baseParameters.maximumShardSlotNumber
-    ],
-    equipEffects: item.definition.equipEffects.map(fx => ({
-      id: fx.effect.definition.actionId,
-      params: fx.effect.definition.params
-    }))
+    set,
+    rarity,
+    shard,
+    equipEffects
   }
 }
 
