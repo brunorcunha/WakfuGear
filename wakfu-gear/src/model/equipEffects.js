d2d567123d7e7a8ae4aa6a65d4aec9f3c8f10557
Bruno Cunha <brunorcunha@hotmail.com>
2020-05-29 16:27:22 -0300

    Last adjust to random elements

--- wakfu-gear/src/model/equipEffects.js
+++ wakfu-gear/src/model/equipEffects.js
@@ -367,49 +367,49 @@ export const equipEffects = [
   {
     id: 910681,
     iid: [],
-    fr: 'Maîtrise sur 1 élément aléatoire',
-    en: 'Mastery of 1 random element',
-    es: 'Dominio de 1 elemento aleatorio',
-    pt: 'Domínio sobre 1 elemento aleatório'
+    fr: 'Maîtrise sur 1 élément',
+    en: 'Mastery of 1 element',
+    es: 'Dominio de 1 elemento',
+    pt: 'Domínio sobre 1 elemento'
   },
   {
     id: 910682,
     iid: [],
-    fr: 'Maîtrise sur 2 éléments aléatoires',
-    en: 'Mastery of 2 random elements',
-    es: 'Dominio de 2 elementos aleatorios',
-    pt: 'Domínio sobre 2 elementos aleatórios'
+    fr: 'Maîtrise sur 2 éléments',
+    en: 'Mastery of 2 elements',
+    es: 'Dominio de 2 elementos',
+    pt: 'Domínio sobre 2 elementos'
   },
   {
     id: 910683,
     iid: [],
-    fr: 'Maîtrise sur 3 éléments aléatoires',
-    en: 'Mastery of 3 random elements',
-    es: 'Dominio de 3 elementos aleatorios',
-    pt: 'Domínio sobre 3 elementos aleatórios'
+    fr: 'Maîtrise sur 3 éléments',
+    en: 'Mastery of 3 elements',
+    es: 'Dominio de 3 elementos',
+    pt: 'Domínio sobre 3 elementos'
   },
   {
     id: 910691,
     iid: [],
-    fr: 'Résistance sur 1 élément aléatoire',
-    en: 'Resistance to 1 random element',
-    es: 'Resistencia a 1 elemento aleatorio',
-    pt: 'Resistência a 1 elemento aleatório'
+    fr: 'Résistance sur 1 élément',
+    en: 'Resistance to 1 element',
+    es: 'Resistencia a 1 elemento',
+    pt: 'Resistência a 1 elemento'
   },
   {
     id: 910692,
     iid: [],
-    fr: 'Résistance sur 2 éléments aléatoires',
-    en: 'Resistance to 2 random elements',
-    es: 'Resistencia a 2 elementos aleatorios',
-    pt: 'Resistência a 2 elementos aleatórios'
+    fr: 'Résistance sur 2 éléments',
+    en: 'Resistance to 2 elements',
+    es: 'Resistencia a 2 elementos',
+    pt: 'Resistência a 2 elementos'
   },
   {
     id: 910693,
     iid: [],
-    fr: 'Résistance sur 3 éléments aléatoires',
-    en: 'Resistance to 3 random elements',
-    es: 'Resistencia a 3 elementos aleatorios',
-    pt: 'Resistência a 3 elementos aleatórios'
+    fr: 'Résistance sur 3 éléments',
+    en: 'Resistance to 3 elements',
+    es: 'Resistencia a 3 elementos',
+    pt: 'Resistência a 3 elementos'
   }
 ]
