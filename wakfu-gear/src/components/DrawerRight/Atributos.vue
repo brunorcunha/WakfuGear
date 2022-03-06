fed4698054c5552156f3a66c282abbe64e92870c
Bruno Cunha <brunorcunha@hotmail.com>
2022-03-06 08:39:11 -0300

    ajuste

--- wakfu-gear/src/components/DrawerRight/Atributos.vue
+++ wakfu-gear/src/components/DrawerRight/Atributos.vue
@@ -208,8 +208,7 @@ export default {
         { id: 162, icone: 'prosp' },
         { id: 184, icone: 'control' },
         { id: 234, icone: 'kit' },
-        { id: 177, icone: 'will' },
-        { id: 9999, icone: 'acumulado' }
+        { id: 177, icone: 'will' }
       ],
       secundario: [
         { id: 149, icone: 'mastcrit' },
